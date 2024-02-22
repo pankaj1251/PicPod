786b6871f0492574b1764eb6fa84f5f7bde8a7f3
Pankaj Pandit <pankajpandit1215@gmail.com>
2024-02-23 00:52:48 +0530

    add profiles, update templates

--- PicPod/views.py
+++ PicPod/views.py
@@ -1,5 +1,103 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+# from itertools import chain
+import random
+
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+@login_required(login_url='signin')
+def index(req):
+    return render(req, 'index.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+#signup
+def signup(req):
+    if req.method == 'POST':
+        username = req.POST['username']
+        email = req.POST['email']
+        password = req.POST['password']
+        password2 = req.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(req, 'Email already exists.')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(req, 'Username already exists.')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password = password)
+                auth.login(req, user_login)
+
+                #create a Profile object for new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user = user_model.id)
+                new_profile.save();
+                return redirect('setting')
+        else:
+            messages.info(req, 'Passwords did not match!')
+            return redirect('signup')
+
+    else:    
+        return render(req, 'signup.html')
+    
+#signin
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+
+#logout
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
